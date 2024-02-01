89c4a385854404fefda149c865fbde94b35beb26
Mykola Paziuk <external.Mykola.Paziuk@se.bosch.com>
2024-02-01 07:44:05 +0200

    added list view and list_item_layour after start

--- app/src/main/java/com/example/gitrobotdreamsleesonsnewhomeasks/MainActivity.kt
+++ app/src/main/java/com/example/gitrobotdreamsleesonsnewhomeasks/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.gitrobotdreamsleesonsnewhomeasks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val items = arrayListOf<ListItem>()
+        repeat(100){
+            items.add(ListItem("", "Element #$it"))
+        }
+        val listView:ListView = findViewById(R.id.listView)
     }
 }
+
+data class ListItem(val image:String, val title:String)
